d9e60f94cc1caa5c0b50d878ea78c0b03bb21851
Ramsaiharibabu <Haribabu91000@gmail.com>
2024-02-21 23:34:45 +0530

    update backend

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 3001;
 require("./db/conn");
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(require("./router/auth"));
 app.get("/", (req, res) => {
   res.send("hello guys i am from server ");
