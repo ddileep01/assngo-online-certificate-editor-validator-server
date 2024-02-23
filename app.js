8edc769fb01c16d1dbe34f15af1d490638a7bb2e
Dileep Kumar Doddi <dileep18052001@gmail.com>
2024-02-24 00:11:56 +0530

    disable env

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 4200;
-const MONGODB_URI = process.env.DATABASE;
-// const PORT = 4200;
+// const PORT = process.env.PORT || 4200;
+// const MONGODB_URI = process.env.DATABASE;
+const PORT = 4200;
 require("./db/conn");
 app.use(
   cors({
@@ -16,20 +16,20 @@ app.use(
 );
 
 app.use(express.json());
-// mongoose.connect(
-//   "mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority"
-// );
+mongoose.connect(
+  "mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority"
+);
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB");
-})
-.catch((err) => {
-  console.error("Error connecting to MongoDB:", err.message);
-});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log("Connected to MongoDB");
+// })
+// .catch((err) => {
+//   console.error("Error connecting to MongoDB:", err.message);
+// });
 app.use(require("./router/auth"));
 app.get("/", (req, res) => {
   res.send("hello guys i am from server ");
--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const DB = process.env.DATABASE
-// const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
+// const DB = process.env.DATABASE
+const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
