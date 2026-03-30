3c5fdba82acaa6fbde8d2daabebbe3bb1d44a0a1
ddileep01 <dileep18052001@gmail.com>
2026-03-30 11:43:34 +0530

    feat: updated mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const cors = require("cors");
 const app = express();
-// const PORT = process.env.PORT || 4200;
-// const MONGODB_URI = process.env.DATABASE;
 const PORT = 4200;
 require("./db/conn");
 app.use(
@@ -17,20 +14,6 @@ app.use(
 );
 
 app.use(express.json());
-mongoose.connect(
-  "mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority"
-);
-
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => {
-//   console.log("Connected to MongoDB");
-// })
-// .catch((err) => {
-//   console.error("Error connecting to MongoDB:", err.message);
-// });
 app.use(require("./router/auth"));
 app.get("/", (req, res) => {
   res.send("hello guys i am from server ");
--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // const DB = process.env.DATABASE
-const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
+// const DB = 'mongodb+srv://haribabu91000:wwnpperHUU2fotQK@cluster0.ykldvtg.mongodb.net/certificateGenerator?retryWrites=true&w=majority'
+const DB = 'mongodb+srv://akhandasevasamsthanass_db_user:jO89CFnqWYwuEV9b@assngo-online-certifica.zywn0ue.mongodb.net/?appName=assngo-online-certificate-editor-validator-server'
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useUnifiedTopology: true
