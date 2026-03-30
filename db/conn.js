b58da17481ab45458ac1f7d8bc69d2faa7fd495b
ddileep01 <dileep18052001@gmail.com>
2026-03-30 12:20:53 +0530

    Revert "feat: updated connection url"
    This reverts commit d444dcefe597dd37b5b558186978fb7b351be6ac.

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB = 'mongodb+srv://akhandasevasamsthanass_db_user:jO89CFnqWYwuEV9b@assngo-online-certifica.zywn0ue.mongodb.net/certificateGenerator?retryWrites=true&w=majority&appName=assngo-online-certificate-editor-validator-server';
+const DB = 'mongodb+srv://akhandasevasamsthanass_db_user:jO89CFnqWYwuEV9b@assngo-online-certifica.zywn0ue.mongodb.net/?appName=assngo-online-certificate-editor-validator-server';
 
 let cached = global.mongoose;
 
